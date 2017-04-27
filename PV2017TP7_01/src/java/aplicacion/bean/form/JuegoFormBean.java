9963a9e57bef7df2a0ca5cfb4c41a7a852b99855
Matías <Matas@192.168.1.2>
2017-04-27 00:01:04 -0300

    Incorporacion del codigo del index con menu, vista del JuegoAhorcado e imagenes del mismo

---
+++ PV2017TP7_01/src/java/aplicacion/bean/form/JuegoFormBean.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion.bean.form;
+
+import aplicacion.modelo.dominio.JuegoAhorcado;
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author Alumno
+ */
+@ManagedBean
+@ViewScoped
+public class JuegoFormBean implements Serializable{
+    private JuegoAhorcado juegoAhorcado;
+    private String palabraIngresada;
+    private char letraIngresada;
+    private boolean componenteDeshabilitado;
+    private String imagen;
+    
+    /**
+     * Creates a new instance of JuegoFormBean
+     */
+    public JuegoFormBean() {
+        juegoAhorcado=new JuegoAhorcado();
+        componenteDeshabilitado = false;
+    }
+    public void empezarJuego(){
+        juegoAhorcado.comenzarJuego(getPalabraIngresada());
+        setComponenteDeshabilitado(true);
+        FacesContext context=FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"JUEGO INICIADO","Tenga Suerte"));
+    }
+    public void controlarLetra(){  
+        boolean esLetraEncontrada=juegoAhorcado.buscarLetra(getLetraIngresada());
+        setImagen(juegoAhorcado.cargarImagen());
+        if (esLetraEncontrada==true){
+            if (juegoAhorcado.juegoGanado()==true){
+                FacesContext context = FacesContext.getCurrentInstance();
+                setComponenteDeshabilitado(false);
+                setPalabraIngresada(new String());
+                setJuegoAhorcado(new JuegoAhorcado());
+                context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"WINER","Usted ha ganado"));
+            }
+        }else {
+            if(juegoAhorcado.getIntentos()==0){
+                FacesContext context = FacesContext.getCurrentInstance();
+                setComponenteDeshabilitado(false);
+                setPalabraIngresada(new String());
+                setJuegoAhorcado(new JuegoAhorcado());
+                context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL,"GAME OVER","Usted ha perdido"));
+            }
+        }
+    }
+
+    /**
+     * @return the juegoAhorcado
+     */
+    public JuegoAhorcado getJuegoAhorcado() {
+        return juegoAhorcado;
+    }
+
+    /**
+     * @param juegoAhorcado the juegoAhorcado to set
+     */
+    public void setJuegoAhorcado(JuegoAhorcado juegoAhorcado) {
+        this.juegoAhorcado = juegoAhorcado;
+    }
+
+    /**
+     * @return the palabraIngresada
+     */
+    public String getPalabraIngresada() {
+        return palabraIngresada;
+    }
+
+    /**
+     * @param palabraIngresada the palabraIngresada to set
+     */
+    public void setPalabraIngresada(String palabraIngresada) {
+        this.palabraIngresada = palabraIngresada;
+    }
+
+    /**
+     * @return the letraIngresada
+     */
+    public char getLetraIngresada() {
+        return letraIngresada;
+    }
+
+    /**
+     * @param letraIngresada the letraIngresada to set
+     */
+    public void setLetraIngresada(char letraIngresada) {
+        this.letraIngresada = letraIngresada;
+    }
+
+    /**
+     * @return the componenteDeshabilitado
+     */
+    public boolean isComponenteDeshabilitado() {
+        return componenteDeshabilitado;
+    }
+
+    /**
+     * @param componenteDeshabilitado the componenteDeshabilitado to set
+     */
+    public void setComponenteDeshabilitado(boolean componenteDeshabilitado) {
+        this.componenteDeshabilitado = componenteDeshabilitado;
+    }
+
+    /**
+     * @return the imagen
+     */
+    public String getImagen() {
+        return imagen;
+    }
+
+    /**
+     * @param imagen the imagen to set
+     */
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+}
---
+++ PV2017TP7_01/src/java/aplicacion/bean/form/RectanguloFormBean.java
@@ -0,0 +1,161 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion.bean.form;
+
+import aplicacion.modelo.dominio.Punto;
+import aplicacion.modelo.dominio.Rectangulo;
+import java.util.ArrayList;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Alumno
+ */
+@ManagedBean
+@ViewScoped
+public class RectanguloFormBean {
+    private double x1;
+    private double y1;
+    private double base;
+    private double altura;
+    private ArrayList<Punto> puntosEncontrados;
+    private Rectangulo unRectangulo;
+    private double superficie;
+    private double perimetro;
+    
+    /**
+     * Creates a new instance of RectanguloFormBean
+     */
+    public RectanguloFormBean() {
+    }
+    public void calcularOtrosPuntos(){
+        Punto unPuntoA = new Punto(getX1(), getY1());
+        Rectangulo rectangulo = new Rectangulo();
+        setPuntosEncontrados(rectangulo.encontrarNuevoPunto(unPuntoA, getBase(), getAltura()));
+        setUnRectangulo(new Rectangulo(getPuntosEncontrados().get(0), getPuntosEncontrados().get(1), getPuntosEncontrados().get(2), getPuntosEncontrados().get(3)));
+    }
+    public double calcularSuperficie(){
+        superficie=altura*base;
+        return superficie;
+    }
+    public double calcularPerimetro(){
+        perimetro=altura*2+base*2;
+        return perimetro;
+    }
+    /**
+     * @return the x1
+     */
+    public double getX1() {
+        return x1;
+    }
+
+    /**
+     * @param x1 the x1 to set
+     */
+    public void setX1(double x1) {
+        this.x1 = x1;
+    }
+
+    /**
+     * @return the y1
+     */
+    public double getY1() {
+        return y1;
+    }
+
+    /**
+     * @param y1 the y1 to set
+     */
+    public void setY1(double y1) {
+        this.y1 = y1;
+    }
+
+    /**
+     * @return the base
+     */
+    public double getBase() {
+        return base;
+    }
+
+    /**
+     * @param base the base to set
+     */
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    /**
+     * @return the altura
+     */
+    public double getAltura() {
+        return altura;
+    }
+
+    /**
+     * @param altura the altura to set
+     */
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    /**
+     * @return the puntosEncontrados
+     */
+    public ArrayList<Punto> getPuntosEncontrados() {
+        return puntosEncontrados;
+    }
+
+    /**
+     * @param puntosEncontrados the puntosEncontrados to set
+     */
+    public void setPuntosEncontrados(ArrayList<Punto> puntosEncontrados) {
+        this.puntosEncontrados = puntosEncontrados;
+    }
+
+    /**
+     * @return the unRectangulo
+     */
+    public Rectangulo getUnRectangulo() {
+        return unRectangulo;
+    }
+
+    /**
+     * @param unRectangulo the unRectangulo to set
+     */
+    public void setUnRectangulo(Rectangulo unRectangulo) {
+        this.unRectangulo = unRectangulo;
+    }
+
+    /**
+     * @return the superficie
+     */
+    public double getSuperficie() {
+        return superficie;
+    }
+
+    /**
+     * @param superficie the superficie to set
+     */
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+
+    /**
+     * @return the perimetro
+     */
+    public double getPerimetro() {
+        return perimetro;
+    }
+
+    /**
+     * @param perimetro the perimetro to set
+     */
+    public void setPerimetro(double perimetro) {
+        this.perimetro = perimetro;
+    }
+
+}
